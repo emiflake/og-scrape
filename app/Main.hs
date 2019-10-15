4ddab6b9e70ce13694fcfd4973c8a1c7f88a0a3e
Emily Martins <emily.flakeheart@gmail.com>
2019-10-15 19:16:04 +0200

    feat: add User-Agent header

--- app/Main.hs
+++ app/Main.hs
@@ -23,9 +23,14 @@ main = do
             url <- param "url"
             htmlContent <- liftAndCatchIO $ do
                 request <- parseRequest url
-                bs <- httpLbs request manager
-                pure . T.decodeUtf8 . responseBody $ bs
-            case getProperties htmlContent of
-                Nothing    -> Web.Scotty.json (errorResponse NoHeadTag "The website has malformed structure")
-                Just []    -> Web.Scotty.json (errorResponse NoMetaData "The website had no metadata")
-                Just props -> Web.Scotty.json $ treeFromPairs props
+                bs <- httpLbs (request {requestHeaders = [("User-Agent","HTTP-Conduit")]}) manager
+                pure . T.decodeUtf8' . responseBody $ bs
+            case htmlContent of
+                Left _ -> Web.Scotty.json $ errorResponse RequestFailed "The response body could not be decoded"
+                Right content -> do
+                    liftIO $ putStrLn $ T.unpack (mconcat ["GETTING ", T.pack url])
+                    liftIO $ putStrLn $ T.unpack content
+                    case getProperties content of
+                        Nothing    -> Web.Scotty.json (errorResponse NoHeadTag "The website has malformed structure")
+                        Just []    -> Web.Scotty.json (errorResponse NoMetaData "The website had no metadata")
+                        Just props -> Web.Scotty.json $ treeFromPairs props
